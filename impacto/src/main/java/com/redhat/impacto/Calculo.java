d88b6363ec4380e1b24ffa81b8358d9fcd307961
brmsAdmin <>
2017-08-30 13:58:39 -0300

    {/impacto/src/main/java/com/redhat/impacto/Calculo.java}

--- impacto/src/main/java/com/redhat/impacto/Calculo.java
+++ impacto/src/main/java/com/redhat/impacto/Calculo.java
@@ -25,6 +25,16 @@ public class Calculo implements java.io.Serializable
 
    private double _totalReceb;
 
+   private double _pesoRelac;
+
+   private double _impacto;
+
+   private double _fatorAbsorcao;
+
+   private double _fatorImpacto;
+
+   private double _scoreFinal;
+
    public Calculo()
    {
    }
@@ -124,6 +134,78 @@ public class Calculo implements java.io.Serializable
       this.ratingDestino = ratingDestino;
       this.setorOrigem = setorOrigem;
       this.pagamento = pagamento;
+   }
+
+   public double get_pesoRelac()
+   {
+      return this._pesoRelac;
+   }
+
+   public void set_pesoRelac(double _pesoRelac)
+   {
+      this._pesoRelac = _pesoRelac;
+   }
+
+   public double get_impacto()
+   {
+      return this._impacto;
+   }
+
+   public void set_impacto(double _impacto)
+   {
+      this._impacto = _impacto;
+   }
+
+   public double get_fatorAbsorcao()
+   {
+      return this._fatorAbsorcao;
+   }
+
+   public void set_fatorAbsorcao(double _fatorAbsorcao)
+   {
+      this._fatorAbsorcao = _fatorAbsorcao;
+   }
+
+   public double get_fatorImpacto()
+   {
+      return this._fatorImpacto;
+   }
+
+   public void set_fatorImpacto(double _fatorImpacto)
+   {
+      this._fatorImpacto = _fatorImpacto;
+   }
+
+   public double get_scoreFinal()
+   {
+      return this._scoreFinal;
+   }
+
+   public void set_scoreFinal(double _scoreFinal)
+   {
+      this._scoreFinal = _scoreFinal;
+   }
+
+   public Calculo(double scoreOrigem, double scoreDestino,
+         java.lang.String segmentoDestino, java.lang.String ratingDestino,
+         java.lang.String setorOrigem, double pagamento,
+         java.util.List<com.redhat.impacto.Recebimento> recebimentos,
+         double _totalReceb, double _pesoRelac, double _impacto,
+         double _fatorAbsorcao, double _fatorImpacto, double _scoreFinal)
+   {
+      this.scoreOrigem = scoreOrigem;
+      this.scoreDestino = scoreDestino;
+      this.segmentoDestino = segmentoDestino;
+      this.ratingDestino = ratingDestino;
+      this.setorOrigem = setorOrigem;
+      this.pagamento = pagamento;
+      this.recebimentos = recebimentos;
+      this._totalReceb = _totalReceb;
+      this._pesoRelac = _pesoRelac;
+      this._impacto = _impacto;
+      this._fatorAbsorcao = _fatorAbsorcao;
+      this._fatorImpacto = _fatorImpacto;
+      this._scoreFinal = _scoreFinal;
    }
 
 }
